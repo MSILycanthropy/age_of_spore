2f39be9e7e04385d9a2f0728d710ebe11b425641
DanielRider <riderDanielJ@gmail.com>
2024-08-17 14:35:38 -0500

    barebones structure for unit

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -3,14 +3,36 @@ extends CharacterBody2D
 @export var isEnemy : bool
 
 var direction = 1
+var health 
+var attack_damage = 1
+# Perhaps Soldier type will be decided upon instancing
+enum SOLDIER_TYPE {MELEE,RANGED,SPECIAL}
+var attack_range
+var screen_size
 
 func _ready():
+	screen_size = get_viewport_rect().size
 	if(isEnemy):
 		direction = -1
 	else: 
 		direction = 1
 		
 	velocity = Vector2(200, 0) * direction
+	
+func _init():
+	# Not sure what the diff between init and ready is yet
+	pass
 
 func _physics_process(delta: float): 
 	move_and_slide()
+	
+func _process(delta):
+	# Main Loop (p much)
+	if health < 0:
+		die()
+	
+	
+	
+func die():
+	hide() # Disappears after dying
+	$CollisionShape2D.set_deferred("disabled", true) # Disable Collision hitbox
