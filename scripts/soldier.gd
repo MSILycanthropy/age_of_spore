5d4ec88bb7b593c098d0a488e2399dd1b0a835c7
DanielRider <riderDanielJ@gmail.com>
2024-08-18 07:44:22 -0500

    class templating for soldier

--- scripts/soldier.gd
+++ scripts/soldier.gd
@@ -1,31 +1,40 @@
 extends Unit
 
+# Melee Soldier Base Class
 
-# Solider Base Class
+@export var export_attack_range : int = 10
+@export var export_attack_damage : int = 10 # damage per attack
+@export var export_attack_speed : int = 10 # ticks per attack
 
-#func _ready():
-	#screen_size = get_viewport_rect().size
-	#if(isEnemy):
-		#direction = -1
-	#else: 
-		#direction = 1
-		#
-	#velocity = Vector2(200, 0) * direction
-	#
-#func _init():
-	## Not sure what the diff between init and ready is yet
-	#pass
-#
-#func _physics_process(delta: float): 
-	#move_and_slide()
-	#
-#func _process(delta):
-	## Main Loop (p much)
-	#if health < 0:
-		#die()
-	#
-	#
-	#
-#func die():
-	#hide() # Disappears after dying
-	#$CollisionShape2D.set_deferred("disabled", true) # Disable Collision hitbox
+class Attack extends RefCounted:
+	var attack_damage : int
+
+	func _init(export_attack_damage = 10):
+		self.attack_damage = export_attack_damage
+
+
+@export var unit_speed : int  = 20 # 20-50 seems decent
+
+var soldier_attack  # Declare soldier_attack as a member variable
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _init():
+	super()
+	if(isEnemy):
+		direction = -1
+	else: 
+		direction = 1
+
+	soldier_attack = Attack.new(export_attack_damage)  # Initialize the member variable
+
+func area_entered(body):
+	set_linear_velocity(Vector2(0,0))
+	print("Collided")
+	print(id)
+	contact.emit()
+	attack_enemy(body)
+
+func attack_enemy(body):
+	body.hp -= soldier_attack.attack_damage  # Access the member variable here
