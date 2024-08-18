805b6557e11391bd3f16cc796e93a5df983e029c
Michael Cu <43478115+michaelcu@users.noreply.github.com>
2024-08-18 16:05:28 -0500

    Soldier Collision and Attack Detection (#10)

---
+++ scripts/attack.gd
@@ -0,0 +1,8 @@
+class_name Attack
+var attack_damage
+var attack_range
+var attack_speed
+func _init(atk_dmg=10, atk_rng=2,atk_spd=10):
+	self.attack_damage = atk_dmg
+	self.attack_range = atk_rng
+	self.attack_speed = atk_spd
--- scripts/unit.gd
+++ scripts/unit.gd
@@ -2,18 +2,19 @@ class_name Unit
 extends RigidBody2D
 signal contact
 
-
-@export var isEnemy : bool
+# Base class variables
+@export var id:int
+@export var isEnemy: bool
+@export var unitSpeed: int = 20  # Between 10-50
 
 
 var direction = 1
-var health 
-var attack_damage = 1
-# Perhaps Soldier type will be decided upon instancing
-# enum SOLDIER_TYPE {MELEE,RANGED,SPECIAL}
+var hp
+var attack
 var attack_range
 var screen_size
-@export var id:int
+
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -21,32 +22,38 @@ func _ready():
 		direction = -1
 	else: 
 		direction = 1
-	set_axis_velocity(Vector2(20,0)*direction)
+	set_axis_velocity(Vector2(40,0)*direction)
 	contact_monitor = true
 	max_contacts_reported = 5
 
-func _init():
-	# Not sure what the diff between init and ready is yet
-	pass
+
+func _init(attack=Attack.new(), hp=10):
+	self.hp = hp
+	self.attack = attack
 	
 	
 func die():
-	hide() # Disappears after dying
-	$CollisionShape2D.set_deferred("disabled", true) # Disable Collision hitbox
-	
+	hide()  # Disappears after dying
+	$CollisionShape2D.set_deferred("disabled", true)  # Disable Collision hitbox
 
-# Collision setup
-# 1. Create Area2d with collision shape rectangle slightly bigger than CollisionShape2d
-# 2. Click Area2d -> Node on top right -> area_entered Signal
-# 3. Set Receiver method to area_entered
+enum unit_type {MELEE,RANGED,SPECIAL}
+
+func get_attack(attack_type):
+	unit_type.MELEE
+
+func attack_enemy(body):
+	print(body.hp)
+	if (isEnemy != body.isEnemy):
+		body.hp -= self.attack.attack_damage  # Access the member variable here
+	print(body.hp)
+
 
 func area_entered(body):
 	set_linear_velocity(Vector2(0,0))
 	print("Collided")
-	print(id)
 	contact.emit()
-	start_attack()
-	#$CollisionShape2D.set_deferred("disabled", true) # Disable Collision hitbox
+	attack_enemy(body.get_parent())
+
 
 func start_attack():
 	pass
