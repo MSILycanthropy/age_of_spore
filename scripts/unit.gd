8c91a3facf2dfd62ca4fce6614b8736e28e382ee
DanielRider <riderDanielJ@gmail.com>
2024-08-17 19:11:07 -0500

    Unit base class coding plus Soldier movement and collision

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -1,14 +1,19 @@
-extends CharacterBody2D
+class_name Unit
+extends RigidBody2D
+signal contact
+
 
 @export var isEnemy : bool
+
 
 var direction = 1
 var health 
 var attack_damage = 1
 # Perhaps Soldier type will be decided upon instancing
-enum SOLDIER_TYPE {MELEE,RANGED,SPECIAL}
+# enum SOLDIER_TYPE {MELEE,RANGED,SPECIAL}
 var attack_range
 var screen_size
+@export var id:int
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -16,23 +21,34 @@ func _ready():
 		direction = -1
 	else: 
 		direction = 1
-		
-	velocity = Vector2(200, 0) * direction
-	
+	set_axis_velocity(Vector2(20,0)*direction)
+	contact_monitor = true
+	max_contacts_reported = 5
+
 func _init():
 	# Not sure what the diff between init and ready is yet
 	pass
-
-func _physics_process(delta: float): 
-	move_and_slide()
-	
-func _process(delta):
-	# Main Loop (p much)
-	if health < 0:
-		die()
-	
 	
 	
 func die():
 	hide() # Disappears after dying
 	$CollisionShape2D.set_deferred("disabled", true) # Disable Collision hitbox
+	
+
+# Collision setup
+# 1. Create Area2d with collision shape rectangle slightly bigger than CollisionShape2d
+# 2. Click Area2d -> Node on top right -> area_entered Signal
+# 3. Set Receiver method to area_entered
+
+func area_entered(body):
+	set_linear_velocity(Vector2(0,0))
+	print("Collided")
+	print(id)
+	contact.emit()
+	start_attack()
+	#$CollisionShape2D.set_deferred("disabled", true) # Disable Collision hitbox
+
+func start_attack():
+	pass
+	
+
